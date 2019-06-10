5dad2b2b4dbdcd7405d43c774d4f98a82af0d393
Aerotek Digital Solution <tim-it@nardoogroup.comgit config --global user.email>
2019-06-10 14:54:25 +0700

    Adding Ballroom Activity

---
+++ app/src/main/java/com/aliyanaresorts/aliyanahotelresorts/activity/MeetingActivity.java
@@ -0,0 +1,16 @@
+package com.aliyanaresorts.aliyanahotelresorts.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.aliyanaresorts.aliyanahotelresorts.R;
+
+public class MeetingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_meeting);
+    }
+}
