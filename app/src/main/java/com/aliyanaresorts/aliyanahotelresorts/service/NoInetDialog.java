eed37573c3320ba85fc0bdac1005e2e0cffe2b00
rahmat <rahmat3nanda@nardoo.com.au>
2019-10-12 13:42:31 +0700

    tes

--- app/src/main/java/com/aliyanaresorts/aliyanahotelresorts/service/NoInetDialog.java
+++ app/src/main/java/com/aliyanaresorts/aliyanahotelresorts/service/NoInetDialog.java
@@ -11,14 +11,13 @@ import java.util.Objects;
 public class NoInetDialog {
 
     private final Activity activity;
-    private Dialog dialog;
 
     public NoInetDialog(Activity activity){
         this.activity = activity;
     }
 
     public void bukaDialog(){
-        dialog  = new Dialog(activity);
+        Dialog dialog = new Dialog(activity);
         dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
         Objects.requireNonNull(dialog.getWindow()).setBackgroundDrawableResource(R.color.transparan);
         dialog.setCancelable(true);
@@ -26,8 +25,5 @@ public class NoInetDialog {
         dialog.show();
     }
 
-    public void tutupDialog(){
-        dialog.dismiss();
-    }
 
 }
